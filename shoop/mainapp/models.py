684336009026d9496c15af00b4888acb4e5c8816
EduardSavlukov <eduardsavlu@gmail.com>
2022-05-13 00:38:34 +0600

    create abstract class

--- shoop/mainapp/models.py
+++ shoop/mainapp/models.py
@@ -1,8 +1,10 @@
 # from unicodedata import category
+# from certifi import contents
 from django.db import models
 # from numpy import product
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -29,6 +31,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     
+    class Meta:
+        abstract = True
+    
     category = models.ForeignKey(Category, verbose_name='Каеугория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
@@ -44,7 +49,10 @@ class CartProduct(models.Model):
     
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    # product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
     final_prace = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Общая стоимость')
     
@@ -73,12 +81,12 @@ class Customer(models.Model):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
     
     
-class Specification(models.Model):
+# class Specification(models.Model):
     
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Наименование товара для характеристик')
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     name = models.CharField(max_length=255, verbose_name='Наименование товара для характеристик')
     
-    def __str__(self) -> str:
-        return "Характеристики для товара: {}".format(self.name)
+#     def __str__(self) -> str:
+#         return "Характеристики для товара: {}".format(self.name)
     
