644a1bcf3b2c071631222ae4c93b576c681ed83b
EduardSavlukov <eduardsavlu@gmail.com>
2022-05-15 01:26:52 +0600

    Create model Smartphon, Create model Notebook

--- shoop/mainapp/models.py
+++ shoop/mainapp/models.py
@@ -44,7 +44,57 @@ class Product(models.Model):
     def __str__(self):
         return self.title
     
+# **************************************
+# Ноутбуки
+# Dioganal
+# Display
+# Processor
+# Ram
+# Video
+# time_without_charge
 
+
+class Notebook(Product):
+    
+    dioganal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы батареии')
+    
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+    
+# **************************************
+
+# Смартфоны
+# Dioganal
+# Display
+# Resolution
+# Acumm_volume
+# Ram
+# sd
+# sd_volume
+# main_cam_mp
+# frontal_cam_mp
+
+class Smartphone(Product):
+    
+    dioganal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolotion =models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    acum_volume = models.CharField(max_length=255,verbose_name='Объем батареи')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume = models.CharField(max_length=255, verbose_name='Объем встроенной памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+    
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+    
+    
 class CartProduct(models.Model):
     
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
